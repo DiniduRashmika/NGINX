93aa824a862ac12a45257ad98a8b7fce11e89b82
DiniduRashmika <dinidurashmika19@gmail.com>
2025-05-16 12:14:29 +0530

    Demo Spring boot project

---
+++ demo_linux/src/test/java/com/example/demo_linux/DemoLinuxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_linux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoLinuxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
